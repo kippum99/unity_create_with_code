0b1fafc3791e58dc9d8f94e31547f970432076c9
June Kim <jooeun@caltech.edu>
2019-11-20 02:57:28 -0800

    Implemented detecting when balls stop moving and resetting cue appropriately

--- 8-Ball/Assets/Scripts/CueController.cs
+++ 8-Ball/Assets/Scripts/CueController.cs
@@ -15,6 +15,12 @@ public class CueController : MonoBehaviour
     private float force = 200;
     private bool shooting = false;
 
+    private bool ballsMoving = false;
+    private float ballsMovingEps = 0.1f;
+
+    private bool cueHit = false;
+    private float timeSinceHit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +31,32 @@ public class CueController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Rotate cue around cue ball
+        // Check if all balls stopped moving
+        if (ballsMoving) {
+            ballsMoving = false;
+            foreach (GameObject ball in GameObject.FindGameObjectsWithTag("Ball")) {
+                Rigidbody ballRb = ball.GetComponent<Rigidbody>();
+                if (ballRb.velocity.magnitude > ballsMovingEps) {
+                    ballsMoving = true;
+                    break;
+                }
+                else {
+                    ballRb.velocity = Vector3.zero;
+                    ballRb.angularVelocity = Vector3.zero;
+                }
+            }
+
+            // Reset cue if balls stopped moving
+            if (!ballsMoving) {
+                // Reset position
+                Debug.Log("Balls stopped moving");
+                Vector3 ballPos = cueBall.transform.position;
+                transform.position = new Vector3(ballPos.x, 2.2931f, ballPos.z - 3.485f);
+                transform.rotation = Quaternion.Euler(5.533f, 0, 0);
+            }
+        }
+
+        // Get user input to rotate cue around cue ball
         horizontalInput = Input.GetAxis("Horizontal");
         transform.RotateAround(cueBall.transform.position, Vector3.up, horizontalInput * rotateAngle);
 
@@ -41,11 +72,19 @@ public class CueController : MonoBehaviour
         if (shooting) {
             rb.AddForce(transform.forward * force);
         }
+
+        // Set ballsMoving after 3 seconds
+        timeSinceHit += Time.deltaTime;
+        if (cueHit && timeSinceHit > 3) {
+            ballsMoving = true;
+        }
     }
 
-    // Stop cue stick when it hits cue ball
+    // Stop cue stick when it hits cue ball and marks balls as moving
     private void OnCollisionEnter(Collision collision) {
         rb.isKinematic = true;
         shooting = false;
+        cueHit = true;
+        timeSinceHit = 0;
     }
 }
