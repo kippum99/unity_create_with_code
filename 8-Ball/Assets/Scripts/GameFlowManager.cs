b70870f3be1967fa90a5012891c82f1a07ccef45
June Kim <jooeun@caltech.edu>
2019-12-11 10:54:46 -0800

    Added 2-player logic

--- 8-Ball/Assets/Scripts/GameFlowManager.cs
+++ 8-Ball/Assets/Scripts/GameFlowManager.cs
@@ -11,9 +11,16 @@ public class GameFlowManager : MonoBehaviour
     public AudioClip booSound;
     public AudioClip applauseSound;
 
+    // These are accessed by CueStickController
     public bool ready = true;       // If player is ready to play
     public bool ballsMoving = false;
+
     private float ballsStopTime = 0;
+    private bool isPlayer1Turn = true;
+    private bool firstBallIn = false;
+    private bool player1IsSolid;
+    private int numSolidsLeft = 7;
+    private int numStripesLeft = 7;
 
     private float cueStickBallDistZ = 3.485f;
     private float cueStickPosY = 2.4f;
@@ -22,12 +29,13 @@ public class GameFlowManager : MonoBehaviour
     private float ballsStopTimeThreshold = 2.5f;
     private float tableHeight = 1.9632f;
     private float tableHeightBelowEps = 0.004f;
-    private float tableHeightEps = 0.003f;
+    private float tableHeightEps = 0.0001f;
     private Vector3 cueBallNewPos = new Vector3(0, 2.5f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
+        PrintPlayerTurn();
     }
 
     // Update is called once per frame
@@ -69,14 +77,17 @@ public class GameFlowManager : MonoBehaviour
         // If game is ready
         else {
             if (!cueStick.activeSelf) {
-                // Reset cue position if cue ball is ready and clear Debug Log
+                // Reset cue position if cue ball is ready
                 if (cueBall.activeSelf && !cueBall.GetComponent<Rigidbody>().isKinematic) {
+                    // Reset cue position
                     Vector3 ballPos = cueBall.transform.position;
                     cueStick.transform.position = new Vector3(ballPos.x, cueStickPosY, ballPos.z - cueStickBallDistZ);
                     cueStick.transform.rotation = Quaternion.Euler(cueStickRotX, 0, 0);
                     cueStick.SetActive(true);
 
-                    Debug.Log("");
+                    // Mark player turn
+                    isPlayer1Turn = !isPlayer1Turn;
+                    PrintPlayerTurn();
                 }
                 else if (!cueBall.activeSelf){
                     // Move cue ball if foul
@@ -112,36 +123,135 @@ public class GameFlowManager : MonoBehaviour
             if (CheckBallIn(ball)) {
                 if (ball == cueBall) {
                     ball.SetActive(false);
-                    Debug.Log("Foul!");
+
+                    string resetPlayer = isPlayer1Turn ? "2" : "1";
+                    Debug.Log("Foul! Player " + resetPlayer + " can place the white ball.");
                     audioSrc.PlayOneShot(booSound, 1);
                 }
                 else if (ball == eightBall) {
                     Destroy(ball);
 
                     // Check if player won or not
-                    int numBalls = GameObject.FindGameObjectsWithTag("Ball").Length;
-                    numBalls -= cueBall.activeSelf ? 1 : 0;
-
-                    if (numBalls > 0) {
-                        audioSrc.PlayOneShot(booSound, 1);
-                        Debug.Log("Game over! You lost :(");
+                    if (isPlayer1Turn) {
+                        if (!firstBallIn) {
+                            Debug.Log("Game over! Player 2 won.");
+                        }
+                        else if (player1IsSolid && numSolidsLeft == 0 || !player1IsSolid && numStripesLeft == 0) {
+                            Debug.Log("Game over! Player 1 won.");
+                        }
+                        else {
+                            Debug.Log("Game over! Player 2 won.");
+                        }
                     }
                     else {
-                        audioSrc.PlayOneShot(applauseSound, 1);
-                        Debug.Log("Game over! You won :)");
+                        if (!firstBallIn) {
+                            Debug.Log("Game over! Player 1 won.");
+                        }
+                        else if (!player1IsSolid && numSolidsLeft == 0 || player1IsSolid && numStripesLeft == 0) {
+                            Debug.Log("Game over! Player 2 won.");
+                        }
+                        else {
+                            Debug.Log("Game over! Player 1 won.");
+                        }
                     }
 
+                    // int numBalls = GameObject.FindGameObjectsWithTag("Ball").Length;
+                    // numBalls -= cueBall.activeSelf ? 1 : 0;
+                    //
+                    // if (numBalls > 0) {
+                    //     audioSrc.PlayOneShot(booSound, 1);
+                    //     Debug.Log("Game over! You lost :(");
+                    // }
+                    // else {
+                    //     audioSrc.PlayOneShot(applauseSound, 1);
+                    //     Debug.Log("Game over! You won :)");
+                    // }
+
                 }
-                else {
+                else {      // Regular ball
+                    bool isSolid = BallIsSolid(ball);
+
+                    if (isSolid) {
+                        numSolidsLeft--;
+
+                        if (firstBallIn) {
+                            if (isPlayer1Turn && player1IsSolid || !isPlayer1Turn && !player1IsSolid) {
+                                audioSrc.PlayOneShot(applauseSound, 1);
+                            }
+                            else {
+                                audioSrc.PlayOneShot(booSound, 1);
+                            }
+                        }
+                    }
+                    else {
+                        numStripesLeft--;
+
+                        if (firstBallIn) {
+                            if (isPlayer1Turn && !player1IsSolid || !isPlayer1Turn && player1IsSolid) {
+                                audioSrc.PlayOneShot(applauseSound, 1);
+                            }
+                            else {
+                                audioSrc.PlayOneShot(booSound, 1);
+                            }
+                        }
+                    }
+
                     Destroy(ball);
-                    audioSrc.PlayOneShot(applauseSound, 1);
+
+                    if (!firstBallIn) {
+                        audioSrc.PlayOneShot(applauseSound, 1);
+
+                        firstBallIn = true;
+
+                        if (isPlayer1Turn) {
+                            if (isSolid) {
+                                player1IsSolid = true;
+                                Debug.Log("Player 1 is solid.");
+                            }
+                            else {
+                                player1IsSolid = false;
+                                Debug.Log("Player 1 is stripe.");
+                            }
+                        }
+                        else {
+                            if (isSolid) {
+                                player1IsSolid = false;
+                                Debug.Log("Player 2 is solid.");
+                            }
+                            else {
+                                player1IsSolid = true;
+                                Debug.Log("Player 2 is stripe.");
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
+    private void PrintPlayerTurn() {
+        string playerNum = isPlayer1Turn ? "1" : "2";
+        string message = "It's Player " + playerNum + "'s turn!";
+
+        if (firstBallIn) {
+            if (isPlayer1Turn && player1IsSolid || !isPlayer1Turn && !player1IsSolid) {
+                message += " You're solid.";
+            }
+            else {
+                message += " You're stripe.";
+            }
+        }
+
+        Debug.Log(message);
+    }
+
     // Check if ball is in pocket
     bool CheckBallIn(GameObject ball) {
         return ball.transform.position.y < tableHeight - tableHeightBelowEps;
+    }
+
+    // Returns true if ball is solid, false if stripe
+    bool BallIsSolid(GameObject ball) {
+        return int.Parse(ball.name.Split(' ')[1]) < 8;
     }
 }
